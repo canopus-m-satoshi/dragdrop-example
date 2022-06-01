d553306ae7b0f3affb2236ef9edda01c1f17d299
Satoshi Matsushima <>
2022-06-01 17:04:51 +0900

    19. ドロップ後の並び替えを実装しよう

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import { Droppable } from 'react-beautiful-dnd'
 import { Draggable } from 'react-beautiful-dnd'
 import { DragDropContext } from 'react-beautiful-dnd'
 import './App.css'
 
 function App() {
+  const [items, setItems] = useState(['item0', 'item1', 'item2'])
+
+  const onDragEnd = (reslut) => {
+    const remove = items.splice(reslut.source.index, 1)
+
+    items.splice(reslut.destination.index, 0, remove[0])
+  }
+
   return (
     <div className="dragDropArea">
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
@@ -17,10 +26,47 @@ function App() {
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}>
-                    item0
+                    {items[0]}
                   </div>
                 )}
               </Draggable>
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId="item1" index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}>
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId="item2" index={2}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}>
+                    {items[2]}
+                  </div>
+                )}
+              </Draggable>
+              {provided.placeholder}
             </div>
           )}
         </Droppable>
